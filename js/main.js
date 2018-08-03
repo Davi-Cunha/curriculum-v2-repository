405a106ec9ea4aedc21f8e3dee6a320cc178e870
Davi-Cunha <davipccunhasilva@hotmail.com>
2018-08-03 04:31:29 -0300

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var div = document.querySelector("#firstDiv");
+var h1 = document.createElement("h1");
+h1.innerHTML = "Initial Commit";
+div.appendChild(h1);
---
+++ ts/main.ts
@@ -0,0 +1,4 @@
+let div = document.querySelector("#firstDiv");
+let h1 = document.createElement("h1");
+h1.innerHTML = "Initial Commit";
+div.appendChild(h1);
