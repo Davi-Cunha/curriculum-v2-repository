edce241462b6e5eb54e69616481d2111f2c0854b
Davi-Cunha <davipccunhasilva@hotmail.com>
2018-08-07 04:27:24 -0300

    Some javascript updated.

--- ts/main.ts
+++ ts/main.ts
@@ -1,11 +1,43 @@
-let backgroundVideo: any = document.querySelector("#background-video");
+// Getting the DOM Elements
+let videoWrap: any = document.querySelector("#video-wrap");
 let titleWrap: any = document.querySelector("#title-wrap");
-let videoHeight: number;
+let titleH1: any = document.querySelector("#title-wrap h1");
+let titleH3: any = document.querySelectorAll("#title-wrap h3");
 
-backgroundVideo.addEventListener('loadedmetadata', () => {
-    videoHeight = backgroundVideo.videoHeight;
-    console.log(videoHeight);
-});
+// Getting the values
+let initialInnerWindowsWidth: number = window.innerWidth;
+let newInnerWidth: number;
+let style: any;
+let fontSize: number;
+
+// Setting the initial margin-top to the title
+titleWrap.style.marginTop = `${videoWrap.clientHeight / 3}px`;
+
+// FIX NEEDED
+// BUG: The H1 font-size shrinks when the window is resized. But it shrinks
+// when both window width and height is resized and it is not supposed to do that.
+window.onresize = event => {
+    titleWrap.style.marginTop = `${videoWrap.clientHeight / 3}px`;
+    newInnerWidth = window.innerWidth;
+
+    // Check if the window is being shrunk or expanded 
+    if (initialInnerWindowsWidth < newInnerWidth) {
+        initialInnerWindowsWidth = newInnerWidth;
+        style = window
+            .getComputedStyle(titleH1, null)
+            .getPropertyValue("font-size");
+        fontSize = parseFloat(style);
+        titleH1.style.fontSize = `${fontSize + 0.5}px`;
+        console.log(fontSize);
+    } else {
+        initialInnerWindowsWidth = newInnerWidth;
+        style = window
+            .getComputedStyle(titleH1, null)
+            .getPropertyValue("font-size");
+        fontSize = parseFloat(style);
+        titleH1.style.fontSize = `${fontSize - 0.5}px`;
+    }
+};
 
 class Contact {
     name: string;
@@ -42,5 +74,3 @@ class ContantManager {
         return 0;
     }
 }
-
-
